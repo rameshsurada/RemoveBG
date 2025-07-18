b4126024b7cd2ed96cfdd330f2ec777047e3910a
name <suradaramesh4@gmail.com>
2025-07-18 22:54:10 +0530

    vercel issues fixed

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
@@ -8,15 +7,30 @@ import { imageRouter } from './routes/imageRoutes.js';
 
 const app = express();
 
-app.use(cors());
+
+app.use(cors({
+  origin: process.env.FRONTEND_URL, //  frontend URL
+  credentials: true,
+}));
+
+
 app.use(express.json());
+
+
 app.use('/api/user', userRouter);
 app.use('/api/image', imageRouter);
+
 
 app.get('/', (req, res) => res.send("API is working"));
 
 const PORT = process.env.PORT || 4000;
 
 ConnectDB()
-  .then(() => app.listen(PORT, () => console.log("Server running on port :",PORT)))
-  .catch(err => console.error(err));
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port: ${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error("Failed to connect to DB:", err);
+  });
